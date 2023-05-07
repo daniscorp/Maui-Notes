3ae33499ff9ef1701cc9a2bfb19f84f1f62bf347
DaniScorp <>
2023-05-07 14:35:24 -0700

    #1 Actualización de la aplicación con conceptos de MVVM

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Proyecto de prueba de Microsoft Learn
 ## Referencias
 
  - [Creación de una aplicación MAUI de .NET](https://learn.microsoft.com/es-mx/dotnet/maui/tutorials/notes-app/?view=net-maui-7.0)
+ - [Actualización de la aplicación con conceptos de MVVM](https://learn.microsoft.com/es-mx/dotnet/maui/tutorials/notes-mvvm/?view=net-maui-7.0)
---
+++ ViewModels/AboutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace Notes.ViewModels
+{
+	public class AboutViewModel
+	{
+        public string Title = AppInfo.Name;
+		public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written By DaniScorp in XAML and C# with .NET MAUI.";
+		public ICommand ShowMoreInfoCommand { get; }
+        public AboutViewModel()
+		{
+			ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+		}
+
+		async Task ShowMoreInfo() => await Launcher.Default.OpenAsync(MoreInfoUrl);
+	}
+}
+
